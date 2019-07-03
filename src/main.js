b0d577ab1dad5e202dc4a3a01196a0f62c230dfb
Eduardo Vogel <11eduardo.vogel@gmail.com>
2019-07-02 23:50:02 -0300

    i18n

--- src/main.js
+++ src/main.js
@@ -4,15 +4,37 @@ import router from './router'
 import Menu from '@/components/Menu.vue'
 import Footer from '@/components/Footer.vue'
 import Subscribe from '@/components/Subscribe.vue'
+import VueI18n from 'vue-i18n'
 
+Vue.use(VueI18n)
 Vue.config.productionTip = false
 Vue.component('menu-custom', Menu)
 Vue.component('footer-custom', Footer)
 Vue.component('subscribe-custom', Subscribe)
 
+const messages = {
+  en: {
+    message: {
+      hello: 'hello world'
+    }
+  },
+  ja: {
+    message: {
+      hello: 'こんにちは、世界'
+    }
+  }
+}
+
+// Create VueI18n instance with options
+const i18n = new VueI18n({
+  locale: 'ja', // set locale
+  messages, // set locale messages
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  i18n,
   router,
   render: h => h(App)
 })
